a915f59600933c27a2705dfe4c7d7194df392eb7
Alessandro Javier Olivares Velarde <68888380+adev-web@users.noreply.github.com>
2022-08-18 09:37:57 +0000

    ingreso de logos redes

--- src/js/layout/index/footer.js
+++ src/js/layout/index/footer.js
@@ -1,48 +1,48 @@
 function createFooter() {
-    const root = document.getElementById('#root');
-    const footer = document.createElement('FOOTER');
-    const div = document.createElement('DIV');
-    const tittle = document.createElement('H4');
-    footer.classList.add('footer');
-    footer.setAttribute('id', 'footer');
-    tittle.innerText = 'derechos reservados a alessandro olivares freelancer 2021';
-    footer.appendChild(div);
-    div.appendChild(tittle);
-    root.appendChild(footer);
-    document.body.append(root);
+  const root = document.getElementById('#root');
+  const footer = document.createElement('FOOTER');
+  const div = document.createElement('DIV');
+  const tittle = document.createElement('H4');
+  footer.classList.add('footer');
+  footer.setAttribute('id', 'footer');
+  tittle.innerText = 'derechos reservados a alessandro olivares freelancer 2021';
+  footer.appendChild(div);
+  div.appendChild(tittle);
+  root.appendChild(footer);
+  document.body.append(root);
 }
 function addRedes() {
-    const Datos = [
-        {
-            icono: 'https://cdn-icons-png.flaticon.com/512/733/733609.png',
-            href: 'https://github.com/adev-web'
-        },
-        {
-            icono: 'https://cdn-icons-png.flaticon.com/512/1384/1384046.png',
-            href: 'https://www.linkedin.com/in/alessandro-olivares-726997204/'
-        },
-        {
-            icono: 'https://cdn-icons-png.flaticon.com/512/1384/1384015.png',
-            href: 'https://www.instagram.com/x.ajov19/'
-        }
-    ]
-    const root = document.getElementById('#root');
-    const footer = document.getElementById('footer');
-    const div = document.createElement('DIV');
-    div.classList.add('footer_links');
-    Datos.forEach(dato => {
-        const link = document.createElement('A');
-        const picture = document.createElement('PICTURE');
-        const img = document.createElement('IMG');
-        link.setAttribute('href', dato.href);
-        img.setAttribute('src', dato.icono);
-        div.appendChild(link);
-        link.appendChild(picture);
-        picture.appendChild(img);
-    });
-    footer.appendChild(div);
-    root.appendChild(footer);
-    document.body.append(root);
+  const Datos = [
+    {
+      icono: '/src/img/logo-github.png',
+      href: 'https://github.com/adev-web'
+    },
+    {
+      icono: '/src/img/logo-linkedin.png',
+      href: 'https://www.linkedin.com/in/alessandro-olivares-726997204/'
+    },
+    {
+      icono: '/src/img/logo-instagram.png',
+      href: 'https://www.instagram.com/x.ajov19/'
+    }
+  ]
+  const root = document.getElementById('#root');
+  const footer = document.getElementById('footer');
+  const div = document.createElement('DIV');
+  div.classList.add('footer_links');
+  Datos.forEach(dato => {
+    const link = document.createElement('A');
+    const picture = document.createElement('PICTURE');
+    const img = document.createElement('IMG');
+    link.setAttribute('href', dato.href);
+    img.setAttribute('src', dato.icono);
+    div.appendChild(link);
+    link.appendChild(picture);
+    picture.appendChild(img);
+  });
+  footer.appendChild(div);
+  root.appendChild(footer);
+  document.body.append(root);
 }
 
 
--- src/js/layout/index/section.js
+++ src/js/layout/index/section.js
@@ -1,63 +1,66 @@
 function CreateSection() {
-    //creacion de elementos
-    const svg_codes = [
-        {
-            svg: '/src/img/icon-html.png',
-            tecnologia: 'HTML',
-        },
-        {
-            svg: '/src/img/icon-css.png',
-            tecnologia: 'CSS',
-        },
-        {
-            svg: '/src/img/icon-js.png',
-            tecnologia: 'JS',
-        },
-        {
-            svg: '/src/img/icon-sql.png',
-            tecnologia: 'SQL', 
-        },
-        {
-            svg: '/src/img/icon-java.png',
-            tecnologia: 'Java', 
-        },
-        {
-            svg: '/src/img/icon-git.png',
-            tecnologia: 'git', 
-        },
-    ];
-    const root = document.getElementById('#root');
-    const section = document.createElement('SECTION');
-    const tittle = document.createElement('H2');
-    const div_Father = document.createElement('DIV');
+  //creacion de elementos
+  const svg_codes = [
+    {
+      svg: '/src/img/icon-html.png',
+      tecnologia: 'HTML',
+    },
+    {
+      svg: '/src/img/icon-css.png',
+      tecnologia: 'CSS',
+    },
+    {
+      svg: '/src/img/icon-git.png',
+      tecnologia: 'git',
+    },
+    {
+      svg: '/src/img/icon-java.png',
+      tecnologia: 'Java',
+    },
+    {
+      svg: '/src/img/icon-sql.png',
+      tecnologia: 'MYSQL',
+    },
+    {
+      svg: '/src/img/icon-js.png',
+      tecnologia: 'JS',
+    },
 
-    svg_codes.forEach(svg_code => {
-        const articule = document.createElement('ARTICULE');
-        const div_Son = document.createElement('DIV');
-        const img_svg = document.createElement('IMG');
-        const paragraph = document.createElement('H3');
-        //asignacion de contenido y classes
-        articule.classList.add('tecnologia');
-        div_Son.classList.add('tecnologia__icono', 'shadow');
-        img_svg.setAttribute('src', svg_code.svg);
-        paragraph.innerText = svg_code.tecnologia;
-        //activacion de herencia
-        div_Son.appendChild(img_svg);
-        articule.appendChild(div_Son);
-        articule.appendChild(paragraph);
-        div_Father.appendChild(articule);
 
-    });
-    //asignacion de contenido
-    section.classList.add('tecnology');
-    section.setAttribute('id', 'tecnology');
-    tittle.classList.add('tecnology__titulo');
-    tittle.innerText = 'Conocimientos en:';
-    div_Father.classList.add('tecnology__tecnologia');
-    //herencia de elementos
-    section.appendChild(tittle);
-    section.appendChild(div_Father);
-    root.appendChild(section);
-    document.body.append(root);
+
+  ];
+  const root = document.getElementById('#root');
+  const section = document.createElement('SECTION');
+  const tittle = document.createElement('H2');
+  const div_Father = document.createElement('DIV');
+
+  svg_codes.forEach(svg_code => {
+    const articule = document.createElement('ARTICULE');
+    const div_Son = document.createElement('DIV');
+    const img_svg = document.createElement('IMG');
+    const paragraph = document.createElement('H3');
+    //asignacion de contenido y classes
+    articule.classList.add('tecnologia');
+    div_Son.classList.add('tecnologia__icono', 'shadow');
+    img_svg.setAttribute('src', svg_code.svg);
+    paragraph.innerText = svg_code.tecnologia;
+    //activacion de herencia
+    div_Son.appendChild(img_svg);
+    articule.appendChild(div_Son);
+    articule.appendChild(paragraph);
+    div_Father.appendChild(articule);
+
+  });
+  //asignacion de contenido
+  section.classList.add('tecnology');
+  section.setAttribute('id', 'tecnology');
+  tittle.classList.add('tecnology__titulo');
+  tittle.innerText = 'Conocimientos en:';
+  div_Father.classList.add('tecnology__tecnologia');
+  //herencia de elementos
+  section.appendChild(tittle);
+  section.appendChild(div_Father);
+  root.appendChild(section);
+  document.body.append(root);
 }
 CreateSection();
