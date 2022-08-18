2c9dfee5212f137908aa2becf91e52c0e8903cec
Alessandro Javier Olivares Velarde <68888380+adev-web@users.noreply.github.com>
2022-08-18 09:19:36 +0000

    responsive design leve

--- src/js/layout/index/header.js
+++ src/js/layout/index/header.js
@@ -1,51 +1,51 @@
 
 function createHeader() {
-    const root = document.getElementById('#root');
-    const header = document.createElement('HEADER');
-    const tittle1 = document.createElement('H1');
-    tittle1.innerText = 'portafolio web';
-    tittle1.classList.add('nav-titulo')
-    root.appendChild(header);
-    header.appendChild(tittle1);
-    document.body.append(root);
+  const root = document.getElementById('#root');
+  const header = document.createElement('HEADER');
+  const tittle1 = document.createElement('H1');
+  tittle1.innerText = 'portafolio web';
+  tittle1.classList.add('nav-titulo')
+  root.appendChild(header);
+  header.appendChild(tittle1);
+  document.body.append(root);
 }
 function createNav() {
-    const navDatos = [
-        {
-            href: 'index.html',
-            tittle: 'Inicio',
-            class: ['nav-list__item', 'boton']
-        },
-        {
-            href: '#tecnologys',
-            tittle: 'Tecnologías',
-            class: ['nav-list__item', 'boton']
-        },
-        {
-            href: '#proyects',
-            tittle: 'Proyectos',
-            class: ['nav-list__item', 'boton']
-        }
-    ];
-    const root = document.getElementById('#root');
-    const div_Father = document.createElement('DIV');
-    div_Father.classList.add('nav-list');
-    navDatos.forEach(dato => {
-        const div_Son = document.createElement('DIV');
-        dato.class.forEach(dato_class => {
-            div_Son.classList.add(dato_class);
-        });
-        const link = document.createElement('A');
-        link.setAttribute("href", dato.href);
-        const tittle = document.createElement('H3');
-        tittle.innerText = dato.tittle;
-
-        link.appendChild(tittle);
-        div_Son.appendChild(link);
-        div_Father.appendChild(div_Son);
-        root.appendChild(div_Father);
-        document.body.append(root);
+  const navDatos = [
+    {
+      href: 'index.html',
+      tittle: 'Inicio',
+      class: ['nav-list__item', 'boton']
+    },
+    {
+      href: '#tecnologys',
+      tittle: 'Tecnologías',
+      class: ['nav-list__item', 'boton']
+    },
+    {
+      href: '#proyects',
+      tittle: 'Proyectos',
+      class: ['nav-list__item', 'boton']
+    }
+  ];
+  const root = document.getElementById('#root');
+  const div_Father = document.createElement('DIV');
+  div_Father.classList.add('nav-list');
+  navDatos.forEach(dato => {
+    const div_Son = document.createElement('DIV');
+    dato.class.forEach(dato_class => {
+      div_Son.classList.add(dato_class);
     });
+    const link = document.createElement('A');
+    link.setAttribute("href", dato.href);
+    const tittle = document.createElement('H3');
+    tittle.innerText = dato.tittle;
+
+    link.appendChild(tittle);
+    div_Son.appendChild(link);
+    div_Father.appendChild(div_Son);
+    root.appendChild(div_Father);
+    document.body.append(root);
+  });
 }
 createHeader();
-createNav();
+//createNav();
